214a98ef28c1e1f14af9c89ad7206f9354b79d79
Yurusalova <Yurusalova@gmail.com>
2019-04-02 22:55:12 +0300

    start HW4

---
+++ RJL.UIP.CPro.HW4.WeatherStatistic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJL.UIP.CPro.HW4.WeatherStatistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
